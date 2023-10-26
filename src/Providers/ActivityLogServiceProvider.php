7251bee57b6887f0a7f48e55c0197258523c6a8d
Corine BOCOGA <cocorine999@gmail.com>
2023-10-26 12:18:51 +0200

    Updates

---
+++ src/Providers/ActivityLogServiceProvider.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace LaravelCoreModule\CoreModuleMaker\Providers;
+
+use Illuminate\Support\ServiceProvider;
+
+use Spatie\Activitylog\ActivitylogServiceProvider as ActivitylogProvider;
+
+class ActivityLogServiceProvider extends ServiceProvider
+{
+    /**
+     * Register services.
+     */
+    public function register(): void
+	{
+        // Use the register method to customize the behavior of the ActivitylogServiceProvider
+        $this->app->register(ActivitylogProvider::class);
+
+        // You can customize the configuration of spatie/laravel-activitylog here
+        $this->mergeConfigFrom(
+            __DIR__.'/../config/activitylog.php',
+            'activitylog'
+        );
+
+        // You can also publish configuration files from your package
+        $this->publishes([
+            __DIR__.'/../config/activitylog.php' => config_path('activitylog.php'),
+        ], 'config');
+    }
+
+    /**
+     * Bootstrap services.
+     */
+    public function boot(): void
+    {
+        $this->publishes([
+            __DIR__.'/../config/myactivitylog.php' => config_path('myactivitylog.php'),
+        ], 'config');
+    }
+}
